126bb14b09651ba9c5f4df377acb7dbb8b7cc6a7
Nikita Filon <132257079+nFeelon@users.noreply.github.com>
2024-02-13 23:35:50 +0300

    Add files via upload

---
+++ lab2/Classes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+
+    [Serializable]
+    public class Owner
+    {
+        public string UserName { get; set; }
+        public string UserSurname { get; set; }
+        public string UserFathername { get; set; }
+        public string PassportNumber { get; set; }
+        public string UserBirthday { get; set; }
+    }
+
+    [Serializable]
+    public class Deposit
+    {
+        public int Number { get; set; }
+        public string DepositType { get; set; }
+        public int Balance { get; set; }
+        public DateTime DateDeposit { get; set; }
+        public bool Ads { get; set; }
+        public bool Banking { get; set; }
+        public bool SMS { get; set; }
+        public string Info { get; set; }
+        public Owner Owner { get; set; }
+    }
+
+    [Serializable]
+    public class Account
+    {
+        public List<Deposit> DepositList { get; set; }
+        public Owner Owner { get; set; }
+        public Account()
+        {
+            Owner = new Owner();
+            DepositList = new List<Deposit>();
+        }
+    }
+}
---
+++ lab2/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace lab2
+{
+    public partial class MainForm : Form
+    {
+
+        public Account banking;
+        public MainForm()
+        {
+            InitializeComponent();
+            banking = new Account();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DepositType.SelectedIndex = 0;
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            Number.Value = 0;
+            DepositType.SelectedIndex = 0; 
+            BalancePicker.Value = 250;
+            DateDeposit.Value = DateTime.Now;
+            AddAds.Checked = false;
+            AddBanking.Checked = false;
+            AddSMS.Checked = false;
+
+            UserName.Text = string.Empty;
+            UserSurname.Text = string.Empty;
+            UserFathername.Text = string.Empty;
+            UserBirthday.SetDate(DateTime.Today);
+            UserPassportNumber.Text = string.Empty;
+
+            InfoList.Items.Clear();
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            if(UserName.Text.Equals("") || UserSurname.Text.Equals("") || UserFathername.Text.Equals("") || UserPassportNumber.Text.Equals("")) {
+                MessageBox.Show("Все поля должны быть заполнены!"); 
+                return;
+            }
+
+            Deposit account = new Deposit()
+            {
+                Number = Int32.Parse(Number.Text),
+                DepositType = DepositType.Text,
+                Balance = BalancePicker.Value,
+                DateDeposit = DateDeposit.Value,
+                Ads = AddAds.Checked,
+                Banking = AddBanking.Checked,
+                SMS = AddSMS.Checked,
+                Owner = new Owner
+                {
+                    UserName = UserName.Text,
+                    UserSurname = UserSurname.Text,
+                    UserFathername = UserFathername.Text,
+                    UserBirthday = UserBirthday.Text
+                }
+            };
+
+            banking.DepositList.Add(account);
+
+            account.Info = "Владелец: " + account.Owner.UserSurname + ", Баланс: " + account.Balance + " рублей, " + "Тип депозита: " + account.DepositType + ", " + "Номер счёта: " + account.Number;
+            InfoList.Items.Add(account.Info);
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Account));
+            using (FileStream stream = new FileStream("Bank.xml", FileMode.OpenOrCreate))
+            {
+                serializer.Serialize(stream, banking);
+            }
+        }
+
+        private void LoadButton_Click(object sender, EventArgs e)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Account));
+            using (FileStream stream = new FileStream("Bank.xml", FileMode.Open))
+            {
+                banking = serializer.Deserialize(stream) as Account;
+            }
+            foreach (Deposit data in banking.DepositList)
+                InfoList.Items.Add(data.Info);
+        }
+    }
+}
